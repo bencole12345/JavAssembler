d647682c4770a34d1f1e13c8642160e90096506a
Ben Cole <bencole12345@gmail.com>
2019-11-12 10:13:53 +0000

    Add sample class to test parser

---
+++ samples/Demo.java
@@ -0,0 +1,12 @@
+public class Demo {
+    public static void main() {
+        int x;
+        x = 12;
+        int y;
+        y = x + 1;
+        int z = addTwelve(y);
+    }
+    public static int addTwelve(int x) {
+        return x + 12;
+    }
+}
