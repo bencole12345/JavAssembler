5439574a17a9b0926c14167206aa3edff2859cd1
Ben Cole <bencole12345@gmail.com>
2019-11-12 10:14:17 +0000

    Modify main method so that AST can be inspected in debugger

--- src/main/java/JavAssembler.java
+++ src/main/java/JavAssembler.java
@@ -1,7 +1,15 @@
+import ast.structure.CompilationUnit;
+import parser.ParserWrapper;
+
+import java.io.IOException;
+
 public class JavAssembler {
 
-    public static void main(String[] args) {
-
+    public static void main(String[] args) throws IOException {
+        if (args.length == 1) {
+            String path = args[0];
+            CompilationUnit unit = ParserWrapper.parseFile(path);
+        }
     }
 
 }
