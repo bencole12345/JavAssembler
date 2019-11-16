2a022417e4deb7a3cab7c5e0ab0211561953de37
Ben Cole <bencole12345@gmail.com>
2019-11-16 17:24:12 +0000

    Add more examples to Demo class

--- samples/Demo.java
+++ samples/Demo.java
@@ -1,4 +1,5 @@
 public class Demo {
+
     public static void main() {
         int x;
         x = 12;
@@ -6,7 +7,34 @@ public class Demo {
         y = x + 1;
         int z = addTwelve(y);
     }
-    public static int addTwelve(int x) {
+
+    private static int addTwelve(int x) {
         return x + 12;
+    }
+
+    public static double doSomething(int a, double b) {
+        double start = 1.0;
+        double soFar = start;
+        for (int i = 0; i < a; i++) {
+            soFar += b * 12;
+        }
+        return soFar;
+    }
+
+    public static int testIfAndWhile(boolean conditionA, boolean conditionB) {
+        int a;
+        if (conditionA) {
+            a = 1;
+        } else if (conditionB) {
+            a = 2;
+        } else {
+            a = 0;
+            int i = 0;
+            while (i < 10) {
+                a *= 10;
+                i++;
+            }
+        }
+        return a;
     }
 }
