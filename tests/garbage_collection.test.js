1c59dcd85b8697951175480bd681bdeb29bcec17
Ben Cole <bencole12345@gmail.com>
2020-04-10 17:07:08 +0100

    Add garbage collection test

---
+++ tests/garbage_collection.test.js
@@ -0,0 +1,21 @@
+const fs = require('fs');
+const path = require('path');
+const wabt = require('wabt')();
+
+let wasmInstance;
+beforeAll(async () => {
+  const watPath = path.resolve(__dirname, '..', 'sample_programs_compiled', 'tests.wat');
+  const watBuffer = fs.readFileSync(watPath, 'utf8');
+  const wasmModule = wabt.parseWat(watPath, watBuffer);
+  const { buffer } = wasmModule.toBinary({});
+  const module = await WebAssembly.compile(buffer);
+  const instance = await WebAssembly.instantiate(module);
+  wasmInstance = instance.exports;
+})
+
+describe('Garbage collection', () => {
+  test('Repeatedly allocate memory', () => {
+    const success = wasmInstance.GarbageCollection_testGarbageCollection(50000);
+    expect(success).toBeTruthy();
+  })
+})
