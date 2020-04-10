8169aba9e69c7ec75608a59df0b915ac1e3e93e1
Ben Cole <bencole12345@gmail.com>
2020-04-10 18:26:10 +0100

    Add tests for array bounds checking

--- tests/arrays.test.js
+++ tests/arrays.test.js
@@ -22,5 +22,25 @@ describe('Arrays', () => {
     const success = wasmInstance.Arrays_testPassArrayAsParameter();
     expect(success).toBeTruthy();
   })
+  test('Accessing array element -1 causes trap', () => {
+    expect(() => {
+      wasmInstance.Arrays_readElementAtIndexInArrayOfSize(-1, 10);
+    }).toThrow();
+  })
+  test('Accessing element 0 of empty array causes trap', () => {
+    expect(() => {
+      wasmInstance.Arrays_readElementAtIndexInArrayOfSize(0, 0);
+    }).toThrow();
+  })
+  test('Accessing element 10 of 10-element array causes trap', () => {
+    expect(() => {
+      wasmInstance.Arrays_readElementAtIndexInArrayOfSize(10, 10);
+    }).toThrow();
+  })
+  test('Accessing element 9 of 10-element array does not cause trap', () => {
+    expect(() => {
+      wasmInstance.Arrays_readElementAtIndexInArrayOfSize(9, 10);
+    }).not.toThrow();
+  })
   // TODO: Add 2D arrays test
 })
