24a69575254faa6b3d0ac0ca60053d9d33dfcc53
Ben Cole <bencole12345@gmail.com>
2020-04-14 09:05:00 +0100

    Add array traversal benchmarks

--- sample_programs/benchmarks/Benchmarks.java
+++ sample_programs/benchmarks/Benchmarks.java
@@ -23,4 +23,14 @@ public class Benchmarks {
             int result = list.popFirstElement();
         }
     }
+
+    public static void traverseArray(int size) {
+        int[] array = new int[size];
+        for (int i = 0; i < size; i++) {
+            array[i] = i;
+        }
+        for (int i = 0; i < size; i++) {
+            int temp = array[i];
+        }
+    }
 }
